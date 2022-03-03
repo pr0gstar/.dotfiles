1ab8223ba1b9a3dfc741e7538f406dbbd38e1a3a
Christoph Planken <c.planken@absturzsicherung.de>
2022-03-03 11:44:32 +0100

    added sublime config and extended setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
