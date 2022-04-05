addd25ca552c81bfa5a2fc8bc532e1a28022dd94
Christoph Planken <c.planken@absturzsicherung.de>
2022-04-05 11:08:02 +0200

    added SublimeText ColorTheme to .dotfiles

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -f -s /.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application Support/Sublime Text 3/Packages/User/Mariana-pr0gstar.sublime-color-scheme
