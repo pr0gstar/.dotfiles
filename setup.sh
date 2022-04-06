803debc0880a9f87abf6d4f680663bd54decdc43
pr0gstar <ch.planken@gmail.com>
2022-04-06 21:05:44 +0200

    added install script for Fonts and fixed Errors in Sublime Settings Path

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -f -s /.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application Support/Sublime Text 3/Packages/User/Mariana-pr0gstar.sublime-color-scheme
+ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
+ln -f -s ~/.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application\ Support/Sublime\ Text/Packages/User/Mariana-pr0gstar.sublime-color-scheme
+cp ~/.dotfiles/font/DankMono/OpenType-PS/* ~/Library/Fonts/
