6abcbd3ced676abe54c088c6706bf0ef18c1376e
pr0gstar <ch.planken@gmail.com>
2022-04-07 20:47:30 +0200

    added hooks to setup script and fixed fint path

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+ln -f -s ~/.dotfiles/hooks/post-checkout ~/.dotfiles/.git/hooks/post-checkout
+ln -f -s ~/.dotfiles/hooks/post-merge ~/.dotfiles/.git/hooks/post-merge
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
 ln -f -s ~/.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application\ Support/Sublime\ Text/Packages/User/Mariana-pr0gstar.sublime-color-scheme
-cp ~/.dotfiles/font/DankMono/OpenType-PS/* ~/Library/Fonts/
+cp ~/.dotfiles/fonts/DankMono/OpenType-PS/* ~/Library/Fonts/
