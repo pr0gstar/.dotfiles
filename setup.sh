76c9f3a7345cd99809977e942fe41e2803bc391b
pr0gstar <ch.planken@gmail.com>
2022-04-07 21:01:30 +0200

    updated install script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,19 @@
+echo ''
+echo '################### Installing Git Hooks ########################'
 ln -f -s ~/.dotfiles/git/hooks/post-checkout .git/hooks/post-checkout
 ln -f -s ~/.dotfiles/git/hooks/post-merge .git/hooks/post-merge
+
+echo ''
+echo '#################### Symlink Config Files #######################'
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
 ln -f -s ~/.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application\ Support/Sublime\ Text/Packages/User/Mariana-pr0gstar.sublime-color-scheme
+
+echo ''
+echo '########## Checkout Fonts from rpivate Github Repo! #############'
+git checkout
+
+echo ''
+echo '################## Adding Fonts to System #######################'
 cp ~/.dotfiles/fonts/DankMono/OpenType-PS/* ~/Library/Fonts/
