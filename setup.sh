1ee0bb28511a52c23eb3a1b6887ccdf79fd448a4
Christoph Planken <c.planken@absturzsicherung.de>
2022-07-06 14:08:57 +0200

    added oh my zsh custom folder to zsh config folder

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ ln -f -s ~/.dotfiles/git/hooks/post-merge .git/hooks/post-merge
 echo ''
 echo '#################### Symlink Config Files #######################'
 ln -f -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -f -s ~/.dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
+ln -f -s ~/.dotfiles/zsh/oh-my-zsh/custom ~/.oh-my-zsh/custom
 ln -f -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 # ln -f -s ~/.dotfiles/sublimetext/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text/Packages/User/Preferences.sublime-settings
 # ln -f -s ~/.dotfiles/sublimetext/Mariana-pr0gstar.sublime-color-scheme ~/Library/Application\ Support/Sublime\ Text/Packages/User/Mariana-pr0gstar.sublime-color-scheme
